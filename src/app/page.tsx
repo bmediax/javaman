786e44190d50694fc6dd86460b379fe5de716343
Brian Moreno <brian.moreno@trailblazers.com>
2023-05-14 10:22:13 -0700

    Updated NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h4>Javaman</h4>
+        <h1>Coming Soon</h1>
+      </div>
+    </main>
+  );
+}
